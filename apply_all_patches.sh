ecb6788a39add1a4e4e63b5ef26f2a9d3f8cd011
Tom <tsiener@gmx.de>
2024-10-07 10:30:13 +0200

    Update apply_all_patches.sh
    nicht benötigte auskommentiert

--- apply_all_patches.sh
+++ apply_all_patches.sh
@@ -14,18 +14,18 @@ echo respondd hopglass
 git apply ../patches/respondd_hopglass.patch
 
 #ecdsasign
-echo "--------------------------------------------------"
-echo ecdsasign
-echo "(wird uebersprungen, da nicht mehr benoetigt)"
+#echo "--------------------------------------------------"
+#echo ecdsasign
+#echo "(wird uebersprungen, da nicht mehr benoetigt)"
 #git apply ../patches/ecdsasign_replace.patch
 
 echo "--------------------------------------------------"
 echo Archer C25v1
 git apply ../patches/archer_C25v1_not_broken.patch
 
-echo "--------------------------------------------------"
-echo Netgear WNR2200
-echo "(wird uebersprungen, da nicht mehr benoetigt)"
+#echo "--------------------------------------------------"
+#echo Netgear WNR2200
+#echo "(wird uebersprungen, da nicht mehr benoetigt)"
 #git apply ../patches/netgear_wnr2200_not_broken.patch
 
 echo "--------------------------------------------------"
@@ -34,9 +34,9 @@ cd openwrt
 git apply ../../patches/futro_cf_card_support.patch
 cd ..
 
-echo "--------------------------------------------------"
-echo MI Router 4a Gigabit Edition
-echo "(wird uebersprungen, da in gluon 2022 nicht mehr benoetigt)"
+#echo "--------------------------------------------------"
+#echo MI Router 4a Gigabit Edition
+#echo "(wird uebersprungen, da in gluon 2022 nicht mehr benoetigt)"
 #pushd ../gluon ; ../patches/add-mi-router-4a-gigabit.sh; popd # adding Xiaomi -mi-router-4a-gigabit edition
 
 echo "--------------------------------------------------"
